e5197570052f144599744c29190576a060617ba6
reemhosny <reemhmahdy@gmail.com>
2019-10-17 10:33:47 +0200

    Navbar SEarch Error

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -15,32 +15,5 @@ export class NavbarComponent implements OnInit {
       firstName: "Reem",
       image: "./assets/images/user.jpeg"
     };
-
-    // document.getElementById("searchIcon").onclick = function() {
-    //   document.getElementById("search").classList.add("visible");
-    //   document.getElementById("clear").classList.add("visible");
-    //   document.getElementById("search").focus();
-    //   document.getElementById("searchIcon").classList.add("hide");
-    // };
-    // document.getElementById("clear").onclick = function() {
-    //   document.getElementById("searchIcon").classList.remove("hide");
-    //   document.getElementById("search").classList.remove("visible");
-    //   document.getElementById("clear").classList.remove("visible");
-    // };
-
-    function searchToggle(obj, evt) {
-      var container = $(obj).closest(".search-wrapper");
-      if (!container.hasClass("active")) {
-        container.addClass("active");
-        evt.preventDefault();
-      } else if (
-        container.hasClass("active") &&
-        $(obj).closest(".input-holder").length == 0
-      ) {
-        container.removeClass("active");
-        // clear input
-        container.find(".search-input").val("");
-      }
-    }
   }
 }
